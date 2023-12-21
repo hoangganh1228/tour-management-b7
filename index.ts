273fc6fba68b5d66c95eaa2b6e12df52de074ac0
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2023-12-21 21:41:51 +0700

    1.5. Lấy ra danh sách Tour

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import sequelize from "./config/database";
+import Tour from "./models/tour.model";
 const app: Express = express();
 
 dotenv.config();
@@ -14,8 +15,19 @@ app.set("view engine", "pug");
 const port: number | string = process.env.PORT || 3000;
 
 
-app.get("/tours", (req: Request, res: Response) => {
-  res.render("client/pages/tours/index");
+app.get("/tours", async (req: Request, res: Response) => {
+  const tours = await Tour.findAll({
+    where: {
+      deleted: false,
+      status: "active"
+    },
+    raw: true
+  });
+  
+  res.render("client/pages/tours/index", {
+    pageTitle: "Danh sách tour",
+    tours: tours
+  });
 });
 
 app.listen(port, () => {
