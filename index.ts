a75e78c3e43b63dd441818f0e8ef7579ce53fd90
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2023-12-23 09:48:42 +0700

    2.3. Cấu trúc lại folder PUG

--- index.ts
+++ index.ts
@@ -2,17 +2,19 @@ import express, { Express } from "express";
 import dotenv from "dotenv";
 import sequelize from "./config/database";
 import clientRoutes from "./routes/client/index.route";
-const app: Express = express();
 
 dotenv.config();
 
 sequelize;
 
+const app: Express = express();
+const port: number | string = process.env.PORT || 3000;
+
+
 
 app.set("views", "./views");
 app.set("view engine", "pug");
 
-const port: number | string = process.env.PORT || 3000;
 
 
 // Routes Client
