43440d89b8445103e940059cbe44827a97e027e4
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2023-12-23 15:02:03 +0700

    1.2. Lấy data in ra giao diện

--- controllers/client/tour.controller.ts
+++ controllers/client/tour.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-// import Tour from "../../models/tour.model";
+import Tour from "../../models/tour.model";
 import sequelize from "../../config/database";
 import { QueryTypes } from "sequelize";
 
@@ -48,9 +48,21 @@ export const index = async (req: Request, res: Response) => {
 export const detail = async (req: Request, res: Response) => {
   const slugTour = req.params.slugTour;
 
-  console.log(slugTour);
+  // console.log(slugTour);
+  const tourDetail = await Tour.findOne({
+    where: {
+      slug: slugTour,
+      deleted: false,
+      status: "active"
+    },
+    raw: true
+  });
+
+  tourDetail["images"] = JSON.parse(tourDetail["images"]);
+  tourDetail["price_special"] = tourDetail["price"] * (1 - tourDetail["discount"]/100);
 
   res.render("client/pages/tours/detail", {
     pageTitle: "Chi tiết tour",
+    tourDetail: tourDetail
   });
 };
--- index.ts
+++ index.ts
@@ -1,11 +1,12 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
-import sequelize from "./config/database";
+// import sequelize from "./config/database";
 import clientRoutes from "./routes/client/index.route";
+import moment from "moment";
 
 dotenv.config();
 
-sequelize;
+// sequelize;
 
 const app: Express = express();
 const port: number | string = process.env.PORT || 3000;
@@ -16,6 +17,8 @@ app.set("views", "./views");
 app.set("view engine", "pug");
 
 
+// App Local Variables
+app.locals.moment = moment;
 
 // Routes Client
 clientRoutes(app);
