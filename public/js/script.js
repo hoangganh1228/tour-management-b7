b177da02d3c42fd7a19af10f21f1b9d018e9f292
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2023-12-23 16:00:22 +0700

    2.1. Thêm tour vào giỏ hàng

--- public/js/script.js
+++ public/js/script.js
@@ -15,3 +15,46 @@ var imagesThumb = new Swiper(".imagesThumb", {
     },
   });
   // End Slider Tour Detail
+
+  
+// Carts
+
+// Kiểm tra xem localStorage có tồn tại cart chưa, nếu chưa thì phải tạo cart mới
+const cart = localStorage.getItem("cart");
+if(!cart) {
+  localStorage.setItem("cart", JSON.stringify([]));
+}
+
+// Thêm tour vào cart
+const formAddToCart = document.querySelector("[form-add-to-cart]");
+if(formAddToCart) {
+  formAddToCart.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const quantity = parseInt(event.target.elements.quantity.value);
+    const tourId = parseInt(formAddToCart.getAttribute("tour-id"));
+
+    // console.log(quantity);
+    // console.log(tourId);
+
+    if(quantity > 0 && tourId) {
+      const cart = JSON.parse(localStorage.getItem("cart"));
+
+      const isExistTour = cart.findIndex(item => item.tourId == tourId);
+
+      if(isExistTour == -1) {
+        cart.push({
+          tourId: tourId,
+          quantity: quantity
+        });
+      } else {
+        cart[isExistTour].quantity = cart[isExistTour].quantity + quantity;
+      }
+
+      console.log(cart);
+      localStorage.setItem("cart", JSON.stringify(cart));
+    }
+  });
+}
+
+// End Carts
