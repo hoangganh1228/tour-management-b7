fc1335e0df47891463f762a951866c51af53458b
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2023-12-24 15:52:59 +0700

    2.1. Lấy thông tin đơn hàng gửi lên server

--- public/js/cart.js
+++ public/js/cart.js
@@ -76,6 +76,42 @@ const deleteItemInCart = () => {
 }
 // Hết Xóa sản phẩm trong giỏ hàng
 
+// Đặt tour
+const formOrder = document.querySelector("[form-order]");
+if(formOrder) {
+  formOrder.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const fullName = event.target.elements.fullName.value;
+    const phone = event.target.elements.phone.value;
+    const note = event.target.elements.note.value;
+
+    const cart = JSON.parse(localStorage.getItem("cart"));
+
+    const data = {
+      info: {
+        fullName: fullName,
+        phone: phone,
+        note: note
+      },
+      cart: cart
+    };
+
+    fetch("/order", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+      })
+  });
+}
+// Hết Đặt tour
+
 // Cập nhật sản phẩm trong giỏ hàng
 const updateQuantityInCart = () => {
   const listInputUpdate = document.querySelectorAll("[list-tour] input[item-id]");
