6f8f724b530ad71e22a86f6322d6f7688a0c1317
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2024-01-02 23:17:40 +0700

    2.2. Bổ sung upload nhiều ảnh

--- routes/admin/tour.route.ts
+++ routes/admin/tour.route.ts
@@ -1,13 +1,24 @@
 import { Router } from "express";
+import multer from "multer";
 const router: Router = Router();
 
 import * as controller from "../../controllers/admin/tour.controller";
+import * as uploadCloud from "../../middlewares/admin/uploadCloud.middleware";
 
 router.get("/", controller.index);
 
 router.get("/create", controller.create);
 
-router.post("/create", controller.createPost);
+const upload = multer();
+
+router.post(
+    "/create",
+    upload.fields([
+      { name: 'images', maxCount: 10 }
+    ]),
+    uploadCloud.uploadFields,
+    controller.createPost
+  );
 
 
 export const tourRoutes: Router = router;
